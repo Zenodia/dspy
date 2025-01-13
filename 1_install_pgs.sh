16a82cda763da47d531819a1d42b4335abf8f758
Zenodia Charpy <zcharpy@nvidia.com>
2025-01-13 11:36:57 +0100

    modify notebook for multi-choices QA and extra package install bash script
    Signed-off-by: Zenodia Charpy <zcharpy@nvidia.com>

--- 1_install_pgs.sh
+++ 1_install_pgs.sh
@@ -1,7 +1,10 @@
-pip install . &&
+#pip install . &&
 pip install openpyxl &&
 pip install colorama &&
 pip install pyarrow==15.0.2 &&
 pip install opencv-fixer==0.2.5 &&
 python -c "from opencv_fixer import AutoFix; AutoFix()" &&
-pip install textworld==1.6.1
+pip install dspy &&
+apt-get update && apt-get install ffmpeg libsm6 libxext6  -y &&
+cp -R  /workspace/dspy/datasets/alfworld/ /usr/local/lib/python3.10/dist-packages/dspy/datasets/
+
